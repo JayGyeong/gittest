b9a8ed59f18f667579289e3e3bdaaaec9b0c43a7
gyeongjin <gyeongjin@kj>
2022-06-13 11:21:50 +0900

    toString

--- gitTest/src/com/gittest/model/vo/Member.java
+++ gitTest/src/com/gittest/model/vo/Member.java
@@ -29,5 +29,11 @@ public class Member {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
+	
 	
 }
